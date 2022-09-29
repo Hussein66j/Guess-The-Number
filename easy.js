e6815e9e47416630342602b95f86fe6c5703aab4
Hussein66j <114643688+Hussein66j@users.noreply.github.com>
2022-09-29 09:20:56 +0300

    Add files via upload

---
+++ easy.js
@@ -0,0 +1,39 @@
+let btn = document.getElementById('btn');
+let btn2 = document.getElementById('btn2');
+let info = document.getElementById('info');
+let input = document.getElementById('input');
+let guesses = document.getElementById('guesses');
+let guessed = document.getElementById('guessed');
+let numberOfGuesses = 0;
+answer = Math.floor(Math.random() * 10);
+btn.addEventListener('click', () => {
+  let num = input.value;
+  if (num > answer) {
+    info.innerHTML = 'The number is lower then that.';
+    info.style.color = '#3064FF';
+    numberOfGuesses ++;
+    guesses.innerHTML = numberOfGuesses;
+    guessed.innerHTML += ',' + num;
+    input.value = '';
+  }
+  else if (num < answer) {
+    info.innerHTML = 'The number is higher then that.';
+    info.style.color = '#FF3434';
+    numberOfGuesses ++;
+    guesses.innerHTML = numberOfGuesses;
+    guessed.innerHTML += ',' + num;
+    input.value = '';
+  }
+  else if (num == answer) {
+    info.innerHTML = 'You won, that is right.';
+    info.style.color = '#00D112';
+    numberOfGuesses ++;
+    guesses.innerHTML = numberOfGuesses;
+    guessed.innerHTML += ',' + num;
+    btn.style.display = 'none';
+    btn2.style.display = 'block';
+  };
+});
+btn2.addEventListener('click', () => {
+  location.reload();
+})
